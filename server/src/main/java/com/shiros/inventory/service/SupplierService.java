46b00e87e498bd5bad988e9271e804c51bc6a0dc
Chris Li <chris@accuenergy.com>
2020-04-24 20:41:59 -0400

    added basic supplier tables

---
+++ server/src/main/java/com/shiros/inventory/service/SupplierService.java
@@ -0,0 +1,15 @@
+package com.shiros.inventory.service;
+
+import com.shiros.inventory.entity.Supplier;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SupplierService {
+
+    public Supplier createSupplier(Supplier supplier);
+    public Optional<Supplier> getSupplier(Long id);
+    public List<Supplier> getAllSuppliers();
+    public Long getSupplierCount();
+
+}
---
+++ server/src/main/java/com/shiros/inventory/service/SupplierServiceImpl.java
@@ -0,0 +1,40 @@
+package com.shiros.inventory.service;
+
+import com.shiros.inventory.entity.Supplier;
+import com.shiros.inventory.repository.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SupplierServiceImpl implements SupplierService {
+
+    SupplierRepository supplierRepository;
+    @Autowired
+    public void setInjectedBean(SupplierRepository supplierRepository) {
+        this.supplierRepository = supplierRepository;
+    }
+
+    @Override
+    public Supplier createSupplier(Supplier supplier) {
+        return supplierRepository.save(supplier);
+    }
+
+    @Override
+    public Optional<Supplier> getSupplier(Long id) {
+        return supplierRepository.findById(id);
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliers() {
+        return (List<Supplier>) supplierRepository.findAll();
+    }
+
+    @Override
+    public Long getSupplierCount() {
+        return supplierRepository.count();
+    }
+
+}
